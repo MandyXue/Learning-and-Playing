c313734ad53658bd1feb1e60d58aa5e341242b04
MandyXue <13564039693@163.com>
2015-08-04 17:04:47 +0800

    add button response, except equal button

--- iOS/MyCalculator/MyCalculator/ViewController.swift
+++ iOS/MyCalculator/MyCalculator/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //展示结果
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    //除了“C”,“=”,“后退”以外的符号响应
+    @IBAction func targetBtn(sender: AnyObject) {
+        let temp = sender.currentTitle!
+        resultLabel.text = resultLabel.text! + temp!
+    }
+    @IBAction func backBtn(sender: AnyObject) {
+        if(resultLabel.text?.lengthOfBytesUsingEncoding(NSUTF16StringEncoding)>0){
+            resultLabel.text?.removeAtIndex((resultLabel.text?.endIndex.predecessor())!)
+        }
+    }
+    @IBAction func equalBtn(sender: AnyObject) {
+    }
+    @IBAction func clearBtn(sender: AnyObject) {
+        resultLabel.text = "";
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

0e9bf3a0f99ae7102e7a4484676bce5ae68bc27c
MandyXue <13564039693@163.com>
2015-08-04 15:37:40 +0800

    calculator initial commit, add auto layout

---
+++ iOS/MyCalculator/MyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by MandyXue on 15/8/4.
+//  Copyright © 2015年 MandyXue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

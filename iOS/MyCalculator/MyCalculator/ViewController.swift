6ea5af39f18a097c116285425a59e2751570c1e5
MandyXue <13564039693@163.com>
2015-08-05 17:07:59 +0800

    improve UI, declare stack struct

--- iOS/MyCalculator/MyCalculator/ViewController.swift
+++ iOS/MyCalculator/MyCalculator/ViewController.swift
@@ -8,9 +8,28 @@
 
 import UIKit
 
+//自定义结构体－－栈（泛性）
+struct Stack<T>{
+    private var items = [T]()
+    
+    mutating func push(item: T) {
+        items.append(item)
+    }
+    mutating func pop() -> T{
+        return items.removeLast()
+    }
+    internal func count() -> Int{
+        return items.count
+    }
+}
+
 class ViewController: UIViewController {
     //展示结果
     @IBOutlet weak var resultLabel: UILabel!
+    
+    //用于设置样式
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBOutlet weak var greenBtn: UIButton!
     
     //除了“C”,“=”,“后退”以外的符号响应
     @IBAction func targetBtn(sender: AnyObject) {
@@ -23,21 +42,38 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func equalBtn(sender: AnyObject) {
-//        TODO: equal button
+        //初始化栈
+        initStack()
     }
     @IBAction func clearBtn(sender: AnyObject) {
         resultLabel.text = "";
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initBtns()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func initStack() -> Stack<Character>{
+        var stack = Stack<Character>()
+        if(resultLabel.text! != ""){
+            for character in resultLabel.text!.characters {
+                stack.push(character)
+            }
+        }
+//        for _ in 0..<stack.count(){
+//            print("stack: \(stack.pop())")
+//        }
+        return stack
     }
-
+    
+    private func initBtns(){
+        for button in buttonCollection{
+            button.layer.borderColor = UIColor(red: 138/255, green: 229/255, blue: 177/255, alpha: 1).CGColor
+            button.layer.borderWidth = 2
+            button.layer.cornerRadius = 15
+        }
+        greenBtn.layer.cornerRadius = 15
+    }
 
 }
 

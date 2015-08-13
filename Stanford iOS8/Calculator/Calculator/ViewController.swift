139f59db63bc0c2bae575e69b36cc3d6246abcdd
MandyXue <13564039693@163.com>
2015-08-13 23:07:49 +0800

    add Stanford iOS8 Lesson 2

--- Stanford iOS8/Calculator/Calculator/ViewController.swift
+++ Stanford iOS8/Calculator/Calculator/ViewController.swift
@@ -22,5 +22,51 @@ class ViewController: UIViewController {
             userIsInTheMiddleOfTypingANumber = true
         }
     }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if(userIsInTheMiddleOfTypingANumber){
+            enter()
+        }
+        switch operation {
+        case "×":performOperation { $0 * $1 }
+        case "÷":performOperation { $1 / $0 }
+        case "+":performOperation { $0 + $1 }
+        case "−":performOperation { $1 - $0 }
+        case "√":performOperation { sqrt($0) }
+        default: break
+        }
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double){
+        if(operateStack.count >= 2){
+            displayValue = operation(operateStack.removeLast(), operateStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double){
+        if(operateStack.count >= 1){
+            displayValue = operation(operateStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operateStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operateStack.append(displayValue)
+        print("operateStack = \(operateStack)")
+    }
+    var displayValue: Double {
+        get{
+            return (NSNumberFormatter().numberFromString(display.text!)?.doubleValue)!
+        }
+        set{
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 

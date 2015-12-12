c3f9ed98e9cf18f9667574156d99e14344f3f947
MandyXue <13564039693@163.com>
2015-12-13 00:14:48 +0800

    finish lesson 12

---
+++ Stanford iOS8/Dropit/Dropit/BezierPathsView.swift
@@ -0,0 +1,13 @@
+//
+//  BezierPathsView.swift
+//  Dropit
+//
+//  Created by MandyXue on 15/12/12.
+//  Copyright © 2015年 MandyXue. All rights reserved.
+//
+
+import Cocoa
+
+class BezierPathsView: UIView {
+
+}
---
+++ Stanford iOS8/Dropit/Dropit/DropitBehavior.swift
@@ -0,0 +1,13 @@
+//
+//  DropitBehavior.swift
+//  Dropit
+//
+//  Created by MandyXue on 15/12/12.
+//  Copyright © 2015年 MandyXue. All rights reserved.
+//
+
+import Cocoa
+
+class DropitBehavior: UIDynamicBehavior {
+
+}
